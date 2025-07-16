8802f1b50a054261af84a2e9b197dadc382c650c
gaangsarr <anjasgangsar12@gmail.com>
2025-07-17 01:55:46 +0700

    pake head untuk icon

---
+++ app/head.js
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>ITPLN Media</title>
+      <meta name="description" content="Website UKM ITPLN Media - 2025" />
+      <link rel="icon" href="/favicon.png" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
