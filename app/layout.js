6f2c42c79308c28f411983b2c8ab6d7c204aa633
gaangsarr <anjasgangsar12@gmail.com>
2025-07-17 01:43:40 +0700

    Fix postcss config and remixicon import

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
 import "remixicon/fonts/remixicon.css";
 
 const fontPoppins = Poppins({
