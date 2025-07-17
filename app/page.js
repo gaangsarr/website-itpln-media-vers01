e091f6134493876f07283deeebdb58a932a19205
gaangsarr <anjasgangsar12@gmail.com>
2025-07-17 22:36:31 +0700

    test foto

--- app/page.js
+++ app/page.js
@@ -25,7 +25,7 @@ export default async function Home() {
         />
       </div>
 
-      {/* Main Content */}
+      {/* Main Contents */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center min-h-screen lg:py-8 -translate-y-3">
           {/* Left Column - Photo Gallery */}
